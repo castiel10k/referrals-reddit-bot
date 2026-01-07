1d46078fad974ae69223edebe2e04741e220811c
castiel10k <71992808+castiel10k@users.noreply.github.com>
2026-01-06 22:15:37 -0500

    updated db

--- information-sample.sql
+++ information-sample.sql
@@ -1,6 +1,4 @@
 BEGIN TRANSACTION;
-
-DROP TABLE IF EXISTS "linkTitleBody";
 DROP TABLE IF EXISTS "referralTitle";
 CREATE TABLE IF NOT EXISTS "referralTitle" (
 	"id"	INTEGER NOT NULL UNIQUE,
@@ -13,14 +11,20 @@ CREATE TABLE IF NOT EXISTS "referralBody" (
 	"body"	INTEGER NOT NULL UNIQUE,
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
-
+DROP TABLE IF EXISTS "subreddit";
+CREATE TABLE IF NOT EXISTS "subreddit" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"name"	TEXT NOT NULL UNIQUE,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+DROP TABLE IF EXISTS "linkTitleBody";
 CREATE TABLE IF NOT EXISTS "linkTitleBody" (
 	"idTitle"	INTEGER NOT NULL UNIQUE,
 	"idBody"	INTEGER NOT NULL UNIQUE,
+	"id"	INTEGER NOT NULL UNIQUE,
+	"isTangerine"	INTEGER NOT NULL DEFAULT 0,
+	PRIMARY KEY("id" AUTOINCREMENT),
 	FOREIGN KEY("idTitle") REFERENCES "referralTitle"("id"),
 	FOREIGN KEY("idBody") REFERENCES "referralBody"("id")
 );
-INSERT INTO "referralTitle" ("id","title") VALUES (1,'Title');
-INSERT INTO "referralBody" ("id","body") VALUES (1,'Body');
-INSERT INTO "linkTitleBody" ("idTitle","idBody") VALUES (1,1);
 COMMIT;
